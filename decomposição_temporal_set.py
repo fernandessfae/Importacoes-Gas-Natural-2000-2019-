d3e2113ec85b00dafeba96cf6294fef5f1bfe662
Aristóteles Fernandes <fernandessfae@hotmail.com>
2020-01-27 16:32:56 -0300

    Decomposição série temporal mensal
    Mostra o comportamento de cada série temporal de cada mês de cada ano

---
+++ decomposição_temporal_set.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from statsmodels.tsa.seasonal import seasonal_decompose
+
+data = pd.read_csv('importacao-gas-natural-2000-2019-m3.csv', delimiter = ';', decimal = ',')
+
+#Anexação do ano no índex da variável para a visualização da série temporal
+dateparse = lambda dates: pd.datetime.strptime(dates, '%Y')
+data = pd.read_csv('importacao-gas-natural-2000-2019-m3.csv', delimiter = ';', decimal = ',',
+                   parse_dates = ['ANO'], index_col ='ANO', date_parser = dateparse)
+
+#Separação da importação de gás natural do mês de setembro
+data_set = data.iloc[:, 11]
+
+#Visualização do gráfico da série temporal de setembro
+plt.figure(figsize = (10, 5))
+plt.xlabel('Anos')
+plt.ylabel('Quantidade de gás natural importado em setembro (mil m³)')
+plt.plot(data_set)
+
+#Decomposição da série temporal do mês de setembro
+decomposicao = seasonal_decompose(data_set)
+tendencia = decomposicao.trend
+sazonal = decomposicao.seasonal
+residuo = decomposicao.resid
+
+#Visualização da tendência, sazionalidade e resíduo da série temporal
+plt.plot(tendencia)
+plt.plot(sazonal)
+plt.plot(residuo)
+
+plt.figure(figsize = (10, 5))
+plt.subplot(4, 1, 1)
+plt.plot(data_set, label = 'Setembro')
+plt.legend(loc = 'best')
+
+plt.subplot(4, 1, 2)
+plt.plot(tendencia, label = 'Tendência')
+plt.legend(loc = 'best')
+
+plt.subplot(4, 1, 3)
+plt.plot(sazonal, label = 'Sazonalidade')
+plt.legend(loc = 'best')
+
+plt.subplot(4, 1, 4)
+plt.plot(residuo, label = 'Residuo')
+plt.legend(loc = 'best')
+plt.tight_layout()
